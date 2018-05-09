11f0bcc9a1eb85b9d9e0885c62933926408d4f13
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 16:19:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.collect do |season, holiday|
-    puts "#{season.to_s}:"
-    holiday.collect do ||
+ 
     
   end
 end
